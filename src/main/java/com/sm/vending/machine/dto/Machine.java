d294243abfcadad073ba4d2b05ed5afdb308f81c
Masoumi-shima <masoumi.shimaa@gmail.com>
2022-11-02 13:59:53 -0400

    Add packages

---
+++ src/main/java/com/sm/vending/machine/dto/Machine.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.sm.vending.machine.dto;
+
+/**
+ *
+ * @author melpomene
+ */
+public class Machine
+{
+    
+}
